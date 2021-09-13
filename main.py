1725118f5023e58e3057b0de2db42baae64e3684
Aidancartney24 <824776@mystma.org>
2021-09-13 15:18:27 -0500

    added top three movies

---
+++ main.py
@@ -0,0 +1,13 @@
+
+#print first movie
+print("revenge of the sith")
+
+#print second movie
+print("Up")
+
+#print third movie
+print("cars")
+
+ git config --global user.email 824776@mystma.org
+  git config --global user.name Aidancartney24
+  
