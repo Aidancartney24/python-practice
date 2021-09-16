0919a839e794c145bd47155aeacb1d9a5b78be8a
Aidancartney24 <824776@mystma.org>
2021-09-16 15:19:22 -0500

    added madlib

--- main.py
+++ main.py
@@ -29,3 +29,28 @@ def topThreevediogames():
     print("Apex legends")
 
 topThreeSports()
+
+def myNameIs(turtle) :
+    print(turtle + "Aidan is my name")
+
+myNameIs("Aidan")
+
+myNameIs("slim shady")
+
+def timesTen(num):
+    print(num * 10)
+
+timesTen(42)
+
+def timesFive(num):
+    print(num * 5)
+
+timesFive(84)
+
+def addTwoNumbas(x, y):
+    print(x + y)
+
+addTwoNumbas(10,59) 
+
+def madLib(instrument, num, day, letter, major, numone, numtwo, adjone, adj, fraction, adj, instrument, adj, noun, faimlymember, stay, adj, lilnasx):
+    print("While Ive stayed at home, Ive practiced the" + instrument + "for" + num + "hours every" + day + ". My favorte key signatyre to play and practice in is" + letter + major + ". It has" + num + "flats and" + num + "sharps. I like this key signature because it is" + adj + "and" + adj + ". Ive also gotten awesome at counting time signatures. The time signature I love is" + fraction + ", because it is" + adj + "to count. One instrument I want to learn how to play while im at home is" + instrument + ", because it is" + adj + "and sounds like a" + noun + ". My" + faimlymember + "likes it when I play" + stay + ", and always gives me a round of applause after my performances! When I return to school, my teacher will be" + adj + "of how great I am at playing my instrument. My teacher might even think I sound ike" + lilnasx + "!")
